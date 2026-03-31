87f29c748d98aebb6d6e7fc47871a353c7938fc3
Evgeny <evg29111@gmail.com>
2026-03-31 22:42:07 +0500

    Изменения в файле

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+   "use strict";
+   
+   const title = '',
+   screenPrice = '',
+   rollback = '',
+   fullPrice = '',
+   adaptive = 5;
+   alert('lesson01');
+   console.log('adaptive: ', adaptive);
+   
+   
+});
