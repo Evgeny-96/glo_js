2d9c76389d75c8c65ab6224205d286183521c8e9
Evgeny <evg29111@gmail.com>
2026-04-02 23:34:39 +0500

    Сохранение перед переходом в lesson03

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
    "use strict";
 
-   const title = 'lesson02',
+   let title = 'lesson02',
       screens = 'Простые, Сложные, Интерактивные',
       screenPrice = 45654,
       rollback = 85,
@@ -18,4 +18,25 @@ document.addEventListener("DOMContentLoaded", () => {
    console.log('screens (нижний регистр, массив): ', screens.toLowerCase().split(", "));
    console.log('процент отката: ', fullPrice * (rollback / 100) + ' %');
 
+   title = prompt("Как называется ваш проект?");
+   screens = prompt("Какие типы экранов нужно разработать?");
+   screenPrice = +prompt("Сколько будет стоить данная работа?", "1200");
+   adaptive = confirm("Нужен ли адаптив на сайте?", "true/false");
+
+   const service1 = prompt("Какой дополнительный тип услуги нужен?"),
+      servicePrice1 = +prompt("Сколько это будет стоить?"),
+      service2 = prompt("Какой дополнительный тип услуги нужен?"),
+      servicePrice2 = +prompt("Сколько это будет стоить?");
+
+   fullPrice = screenPrice + servicePrice1 + servicePrice2;
+   
+   console.log('fullPrice: ', fullPrice);
+
+   const servicePercentPrice = Math.ceil(fullPrice - rollback);
+   console.log('servicePercentPrice: ', servicePercentPrice);
+
+   if (fullPrice >= 30000) console.log("Даем скидку в 10%");
+   else if (fullPrice >= 15000 && fullPrice < 30000) console.log("Даем скидку в 5%");
+   else if (fullPrice < 15000 && fullPrice >= 0) console.log("Скидка не предусмотрена");
+   else console.log("Что то пошло не так");
 });
