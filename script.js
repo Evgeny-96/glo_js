c5a12d8ef40621a40672a214118b2dbf3bd82644
Evgeny <evg29111@gmail.com>
2026-04-20 21:37:35 +0500

    lesson04

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
       fullPrice = 9895,
       adaptive = true;
 
-
-   console.log('title: ', typeof title);
-   console.log('fullPrice: ', typeof fullPrice);
-   console.log('adaptive: ', typeof adaptive);
-   console.log('screens: ', screens.length);
-   console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
-   console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
-   console.log('screens (нижний регистр, массив): ', screens.toLowerCase().split(", "));
-   console.log('процент отката: ', fullPrice * (rollback / 100) + ' %');
-
    title = prompt("Как называется ваш проект?");
    screens = prompt("Какие типы экранов нужно разработать?");
    screenPrice = +prompt("Сколько будет стоить данная работа?", "1200");
@@ -27,16 +17,55 @@ document.addEventListener("DOMContentLoaded", () => {
       servicePrice1 = +prompt("Сколько это будет стоить?"),
       service2 = prompt("Какой дополнительный тип услуги нужен?"),
       servicePrice2 = +prompt("Сколько это будет стоить?");
-
+   
    fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+   // Тип: Function Expression
+   const getAllServicePrices = function(service1, service2) {
+      return service1 + service2;
+   };
+
+   // Тип: Function Declaration
+   function getFullPrice(screenPrice, allServicePrices) {
+      return screenPrice + allServicePrices;
+   };
+
+   function getTitle(text) {
+      if (!text) return '';
+      const cleanTitle = text.trim();
+      return cleanTitle[0].toUpperCase() + cleanTitle.slice(1).toLowerCase();
+   };
+
+   function getServicePercentPrices() {
+      return fullPrice - rollback;
+   };
+
+   function showTypeOf(elem) {
+      console.log(elem, typeof elem);
+   };
+
+   function getRollbackMessage(fullPrice) {
+      if (fullPrice >= 30000) console.log("Даем скидку в 10%");
+      else if (fullPrice >= 15000 && fullPrice < 30000) console.log("Даем скидку в 5%");
+      else if (fullPrice < 15000 && fullPrice >= 0) console.log("Скидка не предусмотрена");
+      else console.log("Что то пошло не так");
+   };
+
+   const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+   fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+   const servicePercentPrice = getServicePercentPrices();
    
    console.log('fullPrice: ', fullPrice);
 
-   const servicePercentPrice = Math.ceil(fullPrice - rollback);
+   
    console.log('servicePercentPrice: ', servicePercentPrice);
 
-   if (fullPrice >= 30000) console.log("Даем скидку в 10%");
-   else if (fullPrice >= 15000 && fullPrice < 30000) console.log("Даем скидку в 5%");
-   else if (fullPrice < 15000 && fullPrice >= 0) console.log("Скидка не предусмотрена");
-   else console.log("Что то пошло не так");
+   showTypeOf(title);
+   showTypeOf(fullPrice);
+   showTypeOf(adaptive);
+   getRollbackMessage(fullPrice);
+   console.log('Название проекта после форматирования', getTitle(title));
+   console.log('Итоговая стоимость за вычетом отката:', servicePercentPrice);
 });
