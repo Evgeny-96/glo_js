442011006c794916393c2a07394abd17986aaf4e
Evgeny-96 <63477629+Evgeny-96@users.noreply.github.com>
2026-06-15 16:36:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,89 @@
 document.addEventListener("DOMContentLoaded", () => {
    "use strict";
 
-   const isNumber = function (numb) {
-      return !isNaN(parseFloat(numb)) && isFinite(numb);
-   }
+   const arrData = {
+      title: '',
+      screens: '',
+      screenPrice: '',
+      adaptive: '',
+      rollback: 10,
+      allServicePrices: 0,
+      fullPrice: 0,
+      servicePercentPrice: 0,
+      i: 0,
+      isNumber: function (num) {
+         return !isNaN(parseFloat(num)) && isFinite(num);
+      },
+      getAllServicePrices: function() {
+         let sum = 0;
 
-   const gameBot = function () {
-      const rundomNumber = Math.floor(Math.random() * 100) + 1;
-
-      function numRecursion () {
-         let numUser = prompt('Угадай число от 1 до 100');
-
-         if (numUser === null) {
-         alert('Игра окончена');
-         return;
-         }
-
-         if (!isNumber(numUser)) {
-            alert('Введи число!');
-            return numRecursion();
+         for (let i = 0; i < 2; i++) {
+            let price;
+            do {
+                  price = prompt("Сколько это будет стоить?");
+            } while (!this.isNumber(price));
+            sum += +price;
          }
          
-         numUser = +numUser;
+         return sum;
+      },
+      showTypeOf: function (variable) {
+         console.log(variable, typeof variable);
+      },
+      getFullPrice: function () {
+         return this.screenPrice + this.allServicePrices;
+      },
+      getServicePercentPrices: function () {
+         return this.fullPrice - (this.fullPrice * (this.rollback / 100));
+      },
+      getTitle: function () {
+         return this.title.trim()[0].toUpperCase() + this.title.trim().slice(1).toLowerCase();
+      },
+      getRollbackMessage: function (price) {
+         if (price >= 30000) return "Даем скидку в 10%";
+         else if (price >= 15000 && price < 30000) return "Даем скидку в 5%";
+         else if (price < 15000 && price >= 0) return "Скидка не предусмотрена";
+         else return "Что то пошло не так";
+      },
+      asking: function () {
+         this.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+         this.screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
          
-         if (numUser > rundomNumber) {
-            alert('Загаданное число меньше');
-            return numRecursion();
-         } else if (numUser < rundomNumber) {
-            alert('Загаданное число больше');
-            return numRecursion();
-         } else if (numUser === rundomNumber) {
-            alert('Поздравляю, Вы угадали!!!');
-            return '';
+         do {
+            this.screenPrice = prompt("Сколько будет стоить данная работа?");
          }
+         while (!this.isNumber(this.screenPrice))
 
+         this.screenPrice = +this.screenPrice;
+         
+         this.adaptive = confirm("Нужен ли адаптив на сайте?");
+      },
+      start: function () {
+         this.asking()
+         this.allServicePrices = this.getAllServicePrices();
+         this.fullPrice = this.getFullPrice();
+         this.servicePercentPrice = this.getServicePercentPrices();
+         this.title = this.getTitle();
+         this.logger();
+      },
+      logger: function () {
+         this.showTypeOf(this.title)
+         this.showTypeOf(this.screenPrice)
+         this.showTypeOf(this.adaptive)
+
+         console.log(this.getRollbackMessage(this.fullPrice))
+         console.log(this.screens.length)
+         console.log(this.servicePercentPrice)
+
+         console.log("Стоимость верстки экранов " + this.screenPrice + " рублей и Стоимость разработки сайта " + this.servicePercentPrice + " рублей")
+         
+         for (let key in this) {
+            if (typeof this[key] !== 'function') {
+               console.log(this[key]);
+            }
+         }         
       }
-
-      return numRecursion;
    }
 
-   const gamebotHero = gameBot();
-   gamebotHero();
- 
+   arrData.start();
 });
